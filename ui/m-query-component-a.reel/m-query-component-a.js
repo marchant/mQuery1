3468b4a513c8d2c3f54e7981d5d01145c43d3c27
Benoit Marchant <benoit@montagestudio.com>
2014-06-08 06:02:08 +0000

    Update files

---
+++ ui/m-query-component-a.reel/m-query-component-a.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/m-query-component-a.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MQueryComponentA
+ * @extends Component
+ */
+exports.MQueryComponentA = Component.specialize(/** @lends MQueryComponentA# */ {
+    constructor: {
+        value: function MQueryComponentA() {
+            this.super();
+        }
+    }
+});
